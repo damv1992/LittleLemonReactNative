9c76fd95224850eb6c00179b66c8964ff84f7965
Daniel Alejandro Miranda Villalta <daniel.alejandro.miranda.villalta@gmail.com>
2024-09-19 17:34:50 -0400

    Construir una pantalla React Native

--- components/LittleLemonFooter.js
+++ components/LittleLemonFooter.js
@@ -3,17 +3,19 @@ import { View, Text } from 'react-native'
 
 export default function LittleLemonFooter() {
     return (
-    <View style={{
+    <View
+      style={{
         backgroundColor: '#F4CE14',
-        // marginBottom: 10,
-    }}>
-        <Text style={{
-            fontSize: 18,
-            color: 'black',
-            textAlign: 'center',
+        marginBottom: 10,
+      }}>
+      <Text
+        style={{
+          fontSize: 18,
+          color: 'black',
+          textAlign: 'center',
         }}>
-            All rights reserved by Little Lemon, 2022{' '}
-        </Text>
+        All rights reserved by Little Lemon, 2022{' '}
+      </Text>
     </View>
-  )
+    )
 }
