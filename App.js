ccc50fcb4daeac8b0977014293d2097551797426
Daniel Alejandro Miranda Villalta <daniel.alejandro.miranda.villalta@gmail.com>
2024-09-19 18:57:17 -0400

    Renderizar una lista grande utilizando FlatList

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import * as React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
 import LittleLemonHeader from './components/LittleLemonHeader'
 import LittleLemonFooter from './components/LittleLemonFooter'
-import WelcomeScreen from './components/WelcomeScreen'
+import MenuItems from './components/MenuItems'
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <WelcomeScreen />
+        <MenuItems />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
