526add5fed0c4479573d3b90072c93c284c65a6d
Daniel Alejandro Miranda Villalta <daniel.alejandro.miranda.villalta@gmail.com>
2024-09-23 18:53:41 -0400

    Establecer rutas

--- App.js
+++ App.js
@@ -4,19 +4,27 @@ import { View, Text, StyleSheet } from 'react-native'
 import LittleLemonHeader from './components/LittleLemonHeader'
 import LittleLemonFooter from './components/LittleLemonFooter'
 import WelcomeScreen from './WelcomeScreen'
-import MenuItems from './components/MenuItems'
 import LoginScreen from './LoginScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <View style={styles.container}>
-        <LittleLemonHeader />
-        <WelcomeScreen />
-      </View>
-      <View style={styles.footerContainer}>
-        <LittleLemonFooter />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
     </>
   )
 }
