f9b8d278d78b3e0cb0cecaf567fba1e4801b5775
Daniel Alejandro Miranda Villalta <daniel.alejandro.miranda.villalta@gmail.com>
2024-09-23 12:43:41 -0400

    Crear un componente TextInput

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import * as React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 import LittleLemonHeader from './components/LittleLemonHeader'
 import LittleLemonFooter from './components/LittleLemonFooter'
-import MenuItems from './components/MenuItems'
+import WelcomeScreen from './WelcomeScreen'
+//import MenuItems from './components/MenuItems'
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <MenuItems />
+        <WelcomeScreen />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
