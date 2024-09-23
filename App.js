dfd75428d4709ba6b530e5cb2286fafd8742b190
Daniel Alejandro Miranda Villalta <daniel.alejandro.miranda.villalta@gmail.com>
2024-09-23 13:12:40 -0400

    Crear una página de inicio de sesión

--- App.js
+++ App.js
@@ -4,14 +4,15 @@ import { View, Text, StyleSheet } from 'react-native'
 import LittleLemonHeader from './components/LittleLemonHeader'
 import LittleLemonFooter from './components/LittleLemonFooter'
 import WelcomeScreen from './WelcomeScreen'
-//import MenuItems from './components/MenuItems'
+import MenuItems from './components/MenuItems'
+import LoginScreen from './LoginScreen'
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <WelcomeScreen />
+        <LoginScreen />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
