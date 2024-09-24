c779008dde836429a2d4b5fb35908e50dbe2b1ae
Daniel Alejandro Miranda Villalta <daniel.alejandro.miranda.villalta@gmail.com>
2024-09-24 12:59:37 -0400

    Suscripción al boletín de Little Lemon

--- App.js
+++ App.js
@@ -1,38 +1,10 @@
-import * as React from 'react'
-import { View, StyleSheet } from 'react-native'
-
-import LittleLemonHeader from './components/LittleLemonHeader'
-import LittleLemonFooter from './components/LittleLemonFooter'
-import WelcomeScreen from './WelcomeScreen'
-import LoginScreen from './LoginScreen'
-import { NavigationContainer } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-
-const Drawer = createDrawerNavigator()
+import { NavigationContainer } from "@react-navigation/native"
+import RootNavigator from "./navigators/RootNavigator"
 
 export default function App() {
   return (
-    <>
-      <NavigationContainer>
-        <View style={styles.container}>
-          <LittleLemonHeader />
-          <Drawer.Navigator initialRouteName="Login">
-            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-            <Drawer.Screen name="Login" component={LoginScreen} />
-          </Drawer.Navigator>
-        </View>
-        <View style={styles.footerContainer}>
-          <LittleLemonFooter />
-        </View>
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333333',
-  },
-  footerContainer: { backgroundColor: '#333333' },
-})
