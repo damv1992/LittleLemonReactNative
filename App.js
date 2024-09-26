a4fbc874b6caff5168e97f49a98ee2107b527341
Daniel Alejandro Miranda Villalta <daniel.alejandro.miranda.villalta@gmail.com>
2024-09-26 16:09:01 -0400

    Leer y escribir con AsyncStorage

--- App.js
+++ App.js
@@ -1,47 +1,103 @@
-import { FlatList, Text, SafeAreaView, View, StyleSheet } from 'react-native'
-import menuItems from './menuItems.json'
+import { useState, useEffect } from 'react'
+import { Text, View, StyleSheet, Alert } from 'react-native'
+import Constants from 'expo-constants'
+import { Switch } from 'react-native-paper'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import useUpdate from './useUpdate'
 
-export default App = () => {
-  const { menu } = menuItems
+export default function App() {
+  const [preferences, setPreferences] = useState({
+    pushNotifications: false,
+    emailMarketing: false,
+    latestNews: false,
+  })
 
-  const Item = ({ name, price }) => (
-    <View style={menuStyles.innerContainer}>
-      <Text style={menuStyles.itemText}>{name}</Text>
-      <Text style={menuStyles.itemText}>{'$' + price}</Text>
-    </View>
-  )
+  useEffect(() => {
+    // Populating preferences from storage using AsyncStorage.multiGet
+    (async () => {
+      try {
+        const values = await AsyncStorage.multiGet(Object.keys(preferences))
+        const initialState = values.reduce((acc, curr) => {
+          // Every item in the values array is itself an array with a string key and a stringified value, i.e ['pushNotifications', 'false']
+          acc[curr[0]] = JSON.parse(curr[1])
+          return acc
+        }, {})
+        setPreferences(initialState)
+      } catch (e) {
+        Alert.alert(`An error occurred: ${e.message}`)
+      }
+    })()
+  }, [])
 
-  const renderItem = ({ item }) => (
-    <Item name={item.title} price={item.price} />
-  )
+  // This effect only runs when the preferences state updates, excluding initial mount
+  useUpdate(() => {
+    (async () => {
+      // Every time there is an update on the preference state, we persist it on storage
+      // The exercise requierement is to use multiSet API
+      const keyValues = Object.entries(preferences).map((entry) => {
+        return [entry[0], String(entry[1])]
+      })
+      try {
+        await AsyncStorage.multiSet(keyValues)
+      } catch (e) {
+        Alert.alert(`An error occurred: ${e.message}`)
+      }
+    })()
+  }, [preferences])
+
+  const updateState = (key) => () =>
+    setPreferences((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }))
 
   return (
-    <SafeAreaView style={menuStyles.container}>
-      <Text style={menuStyles.headerText}>Little Lemon Menu</Text>
-      <FlatList data={menu} keyExtractor={({ id }) => id} renderItem={renderItem} />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.header}>Account Preferences</Text>
+      <View style={styles.row}>
+        <Text style={styles.text}>Push notifications</Text>
+        <Switch
+          value={preferences.pushNotifications}
+          onValueChange={updateState('pushNotifications')}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>Marketing emails</Text>
+        <Switch
+          value={preferences.emailMarketing}
+          onValueChange={updateState('emailMarketing')}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>Latest news</Text>
+        <Switch
+          value={preferences.latestNews}
+          onValueChange={updateState('latestNews')}
+        />
+      </View>
+    </View>
   )
 }
-  
-const menuStyles = StyleSheet.create({
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 16,
   },
-  innerContainer: {
-    paddingHorizontal: 40,
-    paddingVertical: 20,
-    backgroundColor: '#495E57',
+  row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    paddingVertical: 16,
   },
-  itemText: {
-    color: '#F4CE14',
-    fontSize: 22,
+  text: {
+    fontSize: 18,
   },
-  headerText: {
-    color: '#F4CE14',
-    fontSize: 30,
+  header: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
     textAlign: 'center',
-    paddingTop: 30,
   },
 })
